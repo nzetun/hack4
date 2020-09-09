caed49692ef155e090d45f81d4e5d65cfe192931
nzetun <nzetocha2@unl.edu>
2020-09-09 15:14:53 +0000

    updated error messsage

--- retire.c
+++ retire.c
@@ -22,27 +22,30 @@ int main(int argc, char **argv) {
   double inflateRate = atof(argv[4]);
   int yearToRetire = atoi(argv[5]);
   
-  int m = yearToRetire * 12;
   int i=1;
-  double interest, newBal1, newBal2, nestEgg, n, intTotal;
-  double adjReturn = (((1 + returnRate) / (1 + inflateRate)) - 1) / 12;
+  int m = yearToRetire * 12;
+  double interest, newBal1, nestEgg, intTotal;
+  double adjReturn = (((1 + returnRate) / (1 + inflateRate)) - 1) / 12;         //adjusted interest rate based on inflation rate calculation
   newBal1 = startBal;
   
+  if (monthlyAdd >= 1541.67){
+    printf("Illegal: Monthly contributions exceed $18,500 annual Limit\n");
+  } else {
   printf("               401k Table                 \n");
   printf("------------------------------------------\n");
   printf("  Month        Interest          Balance\n");
   
-  for (i>1; i<=m; i++) {
-    interest = adjReturn * newBal1;
-    newBal1 = interest + newBal1;
-    newBal1 += monthlyAdd;
-    intTotal += interest; 
+  for (i>=1; i<=m; i++) {
+    interest = adjReturn * newBal1;                                             //interest adjusted
+    newBal1 = interest + newBal1;                                               //interest added to principle
+    newBal1 += monthlyAdd;                                                      //Monthly addition amount added after interest calculated
+    intTotal += interest;                                                       //Running total of interest
     printf("     %d        $  %.2f        $  %.2f\n", i, interest, newBal1);
-  } 
-
+  }
   nestEgg = newBal1;
   printf("\n  Total Interest Earned :  $  %.2f\n", intTotal);
   printf("  Total Nest Egg:          $  %.2f\n\n", nestEgg);
+  }
   
   return 0;
 }
